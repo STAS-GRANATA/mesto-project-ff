92258969da85f4f6fa8614aa06fff1f25194398f
STAS-GRANATA <147565080+STAS-GRANATA@users.noreply.github.com>
2024-02-04 18:22:20 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const cardList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function createCard(card, deleteFunc) {
 
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
+  deleteButton.addEventListener('click', deleteFunc);
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(evt){
+  const cardDelete = evt.target;
+  cardDelete.closest('.places__item').remove();
+}
 // @todo: Вывести карточки на страницу
+function renderCard(card, container) {
+  const cardElement = createCard(card, deleteCard);
+  container.append(cardElement);
+}
+initialCards.forEach((card) => {
+  renderCard(card, cardList);
+});
